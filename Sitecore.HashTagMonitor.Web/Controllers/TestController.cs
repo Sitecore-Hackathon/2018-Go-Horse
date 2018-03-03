fb389f3aca92a31ae1ef398e80287e2fcdc0df5a
Rodrigo Peplau <rodrigo@peplau.com.br>
2018-03-03 02:27:29 -0300

    Test controller

---
+++ Sitecore.HashTagMonitor.Web/Controllers/TestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Client;
+
+namespace Sitecore.HashTagMonitor.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (XConnectClient client = XConnect.Client.Configuration.SitecoreXConnectClientConfiguration.GetClient())
+            {
+                try
+                {
+                    var contact = new Contact(
+                        new ContactIdentifier("twitter", "myrtlesitecore", ContactIdentifierType.Known)
+                    );
+
+                    client.AddContactIdentifier(contact, new ContactIdentifier("ad-network", "ABC123456", ContactIdentifierType.Anonymous));
+
+                    IReadOnlyCollection<ContactIdentifier> identifiers = contact.Identifiers;
+
+                    client.Submit();
+                }
+                catch (XdbExecutionException ex)
+                {
+                    // Manage exception
+                }
+            }
+
+            return null;
+        }
+
+        public ActionResult AnalyticsRefresh()
+        {
+            if (Analytics.Tracker.Current != null)
+            {
+                Analytics.Tracker.Current.EndTracking();
+                Session.Abandon();
+            }
+            return null;
+        }
+    }
+}
